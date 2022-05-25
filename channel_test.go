18fb75a3f78943262ba56799bc47fb93711e4cc7
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-05-25 08:47:38 +0700

    Membuat channel

---
+++ channel_test.go
@@ -0,0 +1,22 @@
+package goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Nandar Sukmana"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
